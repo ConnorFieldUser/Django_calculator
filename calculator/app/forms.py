c1684f4d080c05c15d6e8b5452e5d752e8b33735
ConnorFieldUser <conquest.field@gmail.com>
2016-10-18 15:42:04 -0400

    commit

---
+++ calculator/app/forms.py
@@ -0,0 +1,7 @@
+#
+# from django import forms
+#
+#
+# class Calc(forms.Form):
+#     num_1 = forms.IntegerField
+#     num_2 = forms.
--- calculator/app/views.py
+++ calculator/app/views.py
@@ -4,6 +4,26 @@ from django.shortcuts import render
 
 
 def index_view(request):
+    print(request.POST)
     context = {
     }
     return render(request, 'index.html', context)
+
+# >>> d = {
+# ...   "number1": 4,
+# ...   "number2": 9,
+# ...   "operator": "+"
+# ... }
+# >>> d
+# {'number2': 9, 'number1': 4, 'operator': '+'}
+# >>>
+# >>> d
+# {'number2': 9, 'number1': 4, 'operator': '+'}
+# >>> # if the operator is a plus, add the two numbers together
+# ...
+# >>> if d['operator'] == '+':
+# ...     new = d['number2'] + d['number1']
+# ...
+# >>> print(new)
+# 13
+# >>>
